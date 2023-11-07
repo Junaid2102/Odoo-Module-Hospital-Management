e45f406a5eebf84530d7467da0ba50876fbebe0d
Junaid2102 <junaidal2102@gmail.com>
2023-11-07 16:16:55 +0500

    Added View files

--- dev/junaid_hospital/__manifest__.py
+++ dev/junaid_hospital/__manifest__.py
@@ -3,4 +3,8 @@
     'author': 'Junaid Alam',
     'website': 'https://junaid2102.github.io/portfolio/',
     'summary': 'Odoo development',
+    'data':[
+        'views/menu.xml',
+        'views/patient.xml',
+    ]
 }
