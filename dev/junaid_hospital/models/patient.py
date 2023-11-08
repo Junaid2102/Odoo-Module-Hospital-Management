34deadf01121704dda1c8feda15d24b4ce30b76c
Junaid2102 <junaidal2102@gmail.com>
2023-11-08 10:58:41 +0500

    Constraints and validation errors

--- dev/junaid_hospital/models/patient.py
+++ dev/junaid_hospital/models/patient.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"            #Create a model
@@ -10,9 +11,23 @@ class HospitalPatient(models.Model):
     age = fields.Integer(string='Age', tracking=True)
     is_child = fields.Boolean(string=' Is Child ?', tracking=True)
     notes = fields.Text(string='Notes', tracking=True)
-    history = fields.Text(string='History', tracking=True)
+    history = fields.Text(string='History', required=True, tracking=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string='Gender', tracking=True)
+    capitalized_name = fields.Char(string='Capitalized Name', compute='compute_capitalized_name', store=True)
 
+    @api.constrains('is_child', 'age')
+    def check_child_age(self):
+        for rec in self:
+            if rec.is_child and rec.age == 0:
+                raise ValidationError('Age has to be recorded !!')
+
+    @api.depends('name')
+    def compute_capitalized_name(self):
+        for rec in self:
+            if rec.name:
+                rec.capitalized_name = rec.name.upper()
+            else:
+                rec.capitalized_name = ''
     @api.onchange('age')
     def on_change_age(self):
         if self.age <= 10:
